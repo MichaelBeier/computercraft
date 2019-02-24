fcd2925cc3dfaebc2beb6db380b6c962e8dfd82a
Bastian Haverkamp <bastian@haverkamp.name>
2019-02-24 17:09:58 +0100

    ae
    Signed-off-by: Bastian Haverkamp <bastian@haverkamp.name>

---
+++ mobfarm/ae.lua
@@ -0,0 +1,64 @@
+function analyeAndMoveConent()
+    local content = analyzeContent();
+    if #content > 0 then
+        moveToChest();
+    end
+    return content;
+end
+
+function moveToTurtle()
+    for i=1, 16 do
+        turtle.suck();
+    end
+end
+
+function moveToChest()
+    for i=1, 16 do
+        turtle.select(i);
+        turtle.drop();
+    end    
+    turtle.select(1);
+end
+
+function sendContentUpdate(content)
+    if #content > 0 then
+        local text = textutils.serialize(content);
+        print(text);
+    end
+end
+
+function analyzeContent()
+    local content = {};
+
+    for i=1, 16 do
+        if turtle.getItemCount(i) == 0 then
+            return content;
+        end
+
+        local data = turtle.getItemDetail(i);
+        local itemName = data.name;
+        local itemCount = data.count;
+
+        local stacked = false
+
+        for j=1, #content do
+            if content[j][1] == itemName then
+                content[j][2] = content[j][2] + itemCount;
+                stacked = true;
+            end
+        end
+
+        if not stacked then
+            table.insert(content, {itemName, itemCount});
+        end
+    end
+
+    return content;
+end
+
+while true do
+    turtle.select(1);
+    os.sleep(1);
+    local content = analyeAndMoveConent();
+    sendContentUpdate(content);
+end
