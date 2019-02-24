41661cd75cfd2b49467627bbcebc049553ed95ac
Bastian Haverkamp <bastian@haverkamp.name>
2019-02-24 16:44:33 +0100

    scheduler

---
+++ mobfarm/scheduler.lua
@@ -0,0 +1,54 @@
+local jobs = {}
+local modemside = "top";
+
+local priomapping = {"user", "AE"};
+
+function startup()
+    rednet.open("top");
+end
+
+function processMessage(message, protocol)
+    if protocol == "jobQuery" then
+        return textutils.serialize(jobs);
+    elseif protocol == "newJob" then
+        local job = textutils.unserialize(message);
+
+        -- {priority ("user", AE"), id, count}
+
+        if #jobs == 0 then
+            local translated = translateJob(job);
+            table.insert(jobs, translated);
+        else
+            local translated = translateJob(job);
+            for i=1, #jobs do
+                if jobs[i][1] > translated[1] then
+                    table.insert(jobs, i, translated);
+                end
+            end
+        end
+    elseif protocol == "currentJob" then
+        if #jobs > 0 then
+            return textutils.serialize(jobs[1]);
+        end
+    end
+end
+
+function getPrio(text)
+    for i = 1, #priomapping do
+        if priomapping[i] == text then
+            return i;
+        end
+    end
+    return 0;
+end
+
+startup();
+while true do
+    local senderID, message, protocol = rednet.receive()
+
+    local response = processMessage(message, protocol);
+
+    if response then
+        rednet.send(senderID, response, protocol);
+    end
+end
